6e5e169108dedfe024a9b9824a4a52d6f4a777c1
IdanZel <idan.zel.harari@gmail.com>
2022-04-16 17:01:49 +0300

    Added 'is enabled' property to clipboard monitor

--- CoordsTool.Core/IO/ClipboardMonitor.cs
+++ CoordsTool.Core/IO/ClipboardMonitor.cs
@@ -10,22 +10,36 @@ public class ClipboardMonitor : IDisposable
     private readonly Action<string> _onClipboardUpdated;
 
     private string _lastClipboardText;
+    private bool _isEnabled;
 
     public ClipboardMonitor(Action<string> onClipboardUpdated)
     {
         _readTimer = new Timer(ReadFromClipboard, null, ReadingInterval, ReadingInterval);
         _onClipboardUpdated = onClipboardUpdated;
         _lastClipboardText = string.Empty;
+        _isEnabled = true;
     }
 
     public void Enable()
     {
+        if (_isEnabled)
+        {
+            return;
+        }
+
         _readTimer.Change(ReadingInterval, ReadingInterval);
+        _isEnabled = true;
     }
 
     public void Disable()
     {
+        if (!_isEnabled)
+        {
+            return;
+        }
+
         _readTimer.Change(Timeout.InfiniteTimeSpan, Timeout.InfiniteTimeSpan);
+        _isEnabled = false;
     }
 
     private async void ReadFromClipboard(object? state)
