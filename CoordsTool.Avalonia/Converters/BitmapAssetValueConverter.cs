3c99137d7c5329cf34e078f7d5f72fe24992b10a
IdanZel <idan.zel.harari@gmail.com>
2022-04-20 15:50:28 +0300

    WIP: Created Avalonia application and started porting WPF to Avalonia

---
+++ CoordsTool.Avalonia/Converters/BitmapAssetValueConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using Avalonia;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+
+namespace CoordsTool.Avalonia.Converters;
+
+/// <summary>
+/// <para>
+/// Converts a string path to a bitmap asset.
+/// </para>
+/// <para>
+/// See: https://github.com/AvaloniaUI/Avalonia/issues/3860#issuecomment-652712504
+/// </para>
+/// </summary>
+public class BitmapAssetValueConverter : IValueConverter
+{
+    public static BitmapAssetValueConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value switch
+        {
+            null => null,
+            string path when targetType == typeof(IImage) => GetBitmapAsset(path),
+            _ => throw new NotSupportedException()
+        };
+    }
+
+    private static object? GetBitmapAsset(string path)
+    {
+        // Using "GetExecutingAssembly" instead of "GetEntryAssembly" to circumvent preview issues
+        var assemblyName = Assembly.GetExecutingAssembly()!.GetName().Name!;
+        var uri = new Uri($"avares://{assemblyName}{path}");
+        var assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
+        return new Bitmap(assets!.Open(uri));
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
---
+++ CoordsTool.Avalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,32 @@
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+
+namespace CoordsTool.Avalonia.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnTitleBarMouseDown(object? sender, PointerPressedEventArgs e)
+        {
+            if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            {
+                BeginMoveDrag(e);
+            }
+        }
+
+        private void OnTextBoxKeyDown(object? sender, KeyEventArgs e)
+        {
+            
+        }
+
+        private void OnSaveCoordinates(object? sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
