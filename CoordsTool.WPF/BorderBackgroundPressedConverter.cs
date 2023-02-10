ebefa077b43a751ae4aefa5fe9d7afc140060f87
IdanZel <idan.zel.harari@gmail.com>
2023-02-10 18:31:59 +0200

    Made buttons UI responsive

---
+++ CoordsTool.WPF/BorderBackgroundPressedConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CoordsTool.WPF;
+
+public class BorderBackgroundPressedConverter : IValueConverter
+{
+    private const double OpacityChange = 0.1;
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not Brush brush)
+        {
+            throw new NotSupportedException();
+        }
+
+        var modifiedBrush = brush.CloneCurrentValue();
+        modifiedBrush.Opacity += OpacityChange;
+
+        return modifiedBrush;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
